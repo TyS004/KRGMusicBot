bbee45b3b96168fc9ec51ab5ab12b7fb729cadf3
Ty <tylerstier13@gmail.com>
2025-01-05 19:21:07 -0800

    Can play youtube audio from link and searching

--- main.py
+++ main.py
@@ -1,19 +1,62 @@
-import os
+import yt_dlp
 
 import discord
 from discord.ext import commands
 
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import os
 
-intents = discord.Intents.default()
+class AudioDL():
+    def __init__(self):
+        self.ydl = yt_dlp.YoutubeDL({'format': 'bestaudio', 'outtmpl': '%(title)s.mp3'})
+        self.video = None
+        self.file_path = ""
 
-intents.message_content = True
-intents.voice_states = True
+    def get_mp3_link(self, link):
+        self.cleanup()
 
-GENERAL_CHANNEL_ID = 996703826520309810
+        self.video = self.ydl.extract_info(link, download=True)
+        self.file_path = self.ydl.prepare_filename(self.video)
+        print(f"downloaded {self.file_path}")
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+        return self.video
+
+    def get_mp3_q(self, q):
+        self.cleanup()
+
+        self.video = self.ydl.extract_info(f"ytsearch:{q}", download=True)
+        for data in os.walk('D:\Python Projects\DiscordBot'):  # where to start searching
+            dir_path, folders, files = data
+
+            for f in files:
+                if f.lower().endswith('.mp3'):
+                    self.file_path = os.path.join(dir_path, f)
+        print(f"downloaded {self.file_path}")
+
+        return self.video
+
+    def get_file_path(self):
+        return self.file_path
+
+    def cleanup(self):
+        if os.path.exists('D:\Python Projects\DiscordBot\\' + self.file_path):
+            os.remove(self.file_path)
+
+class Client_Bot():
+    def __init__(self):
+        self.intents = discord.Intents.default()
+        self.intents.message_content = True
+        self.intents.voice_states = True
+
+        self.GENERAL_CHANNEL_ID = 1325325286807572572
+
+    def create_bot(self):
+        return commands.Bot(command_prefix='$', intents=self.intents)
+
+    def get_channel_id(self):
+        return self.GENERAL_CHANNEL_ID
+
+client_bot = Client_Bot()
+bot = client_bot.create_bot()
 
 @bot.event
 async def on_ready():
@@ -22,7 +65,7 @@ async def on_ready():
 @bot.command()
 async def connect(ctx):
     await ctx.send("Connecting...")
-    general_channel = bot.get_channel(GENERAL_CHANNEL_ID)
+    general_channel = bot.get_channel(client_bot.get_channel_id())
     await general_channel.connect()
 
 @bot.command()
@@ -30,14 +73,25 @@ async def disconnect(ctx):
     if ctx.voice_client:
         await ctx.voice_client.disconnect()
     else:
-        await ctx.send("Not currently in Voice Channel.")
+        await ctx.send("Not in Voice Channel")
 
 @bot.command()
-async def play(ctx):
-    if ctx.voice_client:
-        audio_file = discord.FFmpegPCMAudio("YN Jay x Louie Ray - Triple S (Official Music Video).mp3")
-        await ctx.voice_client.play(audio_file)
+async def play(ctx, args=None):
+    if args:
+        if ctx.voice_client:
+            mp3create = AudioDL()
+
+            song = mp3create.get_mp3_q(args)
+            file_path = mp3create.get_file_path()
+
+            audio_file = discord.FFmpegPCMAudio(file_path)
+
+            ctx.voice_client.play(audio_file)
+
+            await ctx.send("Playing " + song['title'])
+        else:
+            await ctx.send("Not currently in Voice Channel.")
     else:
-        await ctx.send("Not currently in Voice Channel.")
+        await ctx.send("No Song Title")
 
 bot.run(os.environ['TOKEN'])
